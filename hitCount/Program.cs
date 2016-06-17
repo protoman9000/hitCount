bf6b154d4dc497076802c3352b692aa86d1131b3
protoman9000 <oyekan900@yahoo.com>
2016-06-16 23:05:33 -0700

    Completed, going to test it.

--- hitCount/Program.cs
+++ hitCount/Program.cs
@@ -11,7 +11,26 @@ namespace hitCount
         static void Main(string[] args)
         {
             Random die = new Random();
+            int enemy = 1;
+            int attackNow = 0;
+
             int dieNumber = die.Next(1, 5);
+            int attack = dieNumber;
+            attackNow += attack;
+            dieNumber = die.Next(1, 5);
+            while (dieNumber > attack)
+            {
+                attackNow += attack;
+                attack = dieNumber;
+                dieNumber = die.Next(1, 5);
+            }
+
+            int result = enemy - attackNow;
+
+            if (enemy <= 0)
+                Console.WriteLine("The enemy is defeated");
+            else
+                Console.WriteLine("You lost, try again");
         }
     }
 }
