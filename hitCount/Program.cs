c0d1eaa9c1b8a4b8e236e8365295674e42148c9f
protoman9000 <oyekan900@yahoo.com>
2016-06-16 22:46:55 -0700

    starting out

---
+++ hitCount/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hitCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random die = new Random();
+            int dieNumber = die.Next(1, 5);
+        }
+    }
+}
